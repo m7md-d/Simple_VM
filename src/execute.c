4f2817cdb283e3f559808ddc9265d6594eb7df22
Bandar Alsubhi <bandarhatim11@gmail.com>
2026-06-04 16:57:40 +0300

    exe edit

---
+++ src/execute.c
@@ -0,0 +1,53 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+bool opcode_add (vm *vm, InstructionOpcode op, uint16_t rugster_type,
+                 uint16_t r0, uint16_t r1, uint16_t *out)
+{
+ 
+    (void)vm; (void)op; (void)register_type;
+    if (!out) return false;
+    *out = r0 + r1;
+    return true;
+
+}
+
+bool opcode_sub (vm *vm, InstructionOpcode op, uint16_t rugster_type,
+                 uint16_t r0, uint16_t r1, uint16_t *out)
+{
+ 
+    (void)vm; (void)op; (void)register_type;
+    if (!out) return false;
+    *out = r0 - r1;
+    return true;
+
+}
+bool opcode_dw (vm *vm, InstructionOpcode op, uint16_t rugster_type,
+                 uint16_t r0, uint16_t r1, uint16_t *out)
+{}
+bool opcode_sw (vm *vm, InstructionOpcode op, uint16_t rugster_type,
+                 uint16_t r0, uint16_t r1, uint16_t *out)
+{}
+bool opcode_or (vm *vm, InstructionOpcode op, uint16_t rugster_type,
+                 uint16_t r0, uint16_t r1, uint16_t *out)
+{
+ 
+    (void)vm; (void)op; (void)register_type;
+    if (!out) return false;
+    *out = r0 | r1;
+    return true;
+
+}
+bool opcode_not (vm *vm, InstructionOpcode op, uint16_t rugster_type,
+                 uint16_t r0, uint16_t r1, uint16_t *out)
+{
+ 
+    (void)vm; (void)op; (void)register_type;
+    if (!out) return false;
+    *out = ~r0;
+    return true;
+
+}
+bool opcode_jump (vm *vm, InstructionOpcode op, uint16_t rugster_type,
+                 uint16_t r0, uint16_t r1, uint16_t *out)
+{}
